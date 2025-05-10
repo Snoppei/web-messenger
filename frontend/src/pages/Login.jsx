b553ff70c4d841ce51823882081507ca60080537
Snoppei <44434924+Snoppei@users.noreply.github.com>
2025-05-10 05:00:42 +0300

    frontend: add auth pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,47 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+
 const Login = () => {
-    return <>Login</>;
-}
+  return (
+    <>
+      <Form>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "10%",
+            paddingBottom: "10%",
+          }}
+        >
+          <Col xs={5}>
+            <Stack gap={3}>
+              <h2>Авторизация</h2>
+
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="password" placeholder="Пароль" />
+              <Button variant="primary" type="submit">
+                Войти
+              </Button>
+
+              <Alert
+                variant="danger"
+                style={{
+                  padding: "10px",
+                }}
+              >
+                <p
+                  style={{
+                    marginBottom: "0",
+                  }}
+                >
+                  Возникла ошибка
+                </p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,48 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+
 const Register = () => {
-    return <>Register</>;
-}
+  return (
+    <>
+      <Form>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "10%",
+            paddingBottom: "10%",
+          }}
+        >
+          <Col xs={5}>
+            <Stack gap={3}>
+              <h2>Регистрация</h2>
+
+              <Form.Control type="text" placeholder="ФИО" />
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="password" placeholder="Пароль" />
+              <Button variant="primary" type="submit">
+                Зарегистрироваться
+              </Button>
+
+              <Alert
+                variant="danger"
+                style={{
+                  padding: "10px",
+                }}
+              >
+                <p
+                  style={{
+                    marginBottom: "0",
+                  }}
+                >
+                  Возникла ошибка
+                </p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
 
 export default Register;
