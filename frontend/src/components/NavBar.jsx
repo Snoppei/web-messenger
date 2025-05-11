a0e3a3531cc7defbe5beb17f53dbff40d6e2f3ae
Snoppei <44434924+Snoppei@users.noreply.github.com>
2025-05-11 23:55:36 +0300

    frontend: add logout

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,23 +4,49 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 const NavBar = () => {
+  const { user, logoutUser } = useContext(AuthContext);
 
-    const {user} = useContext(AuthContext);
+  return (
+    <Navbar bg="dark" className="mb-4" style={{ height: "3.75rem" }}>
+      <Container>
+        <h2>
+          <Link to="/" className="link-light text-decoration-none">
+            Мессенджер{" "}
+          </Link>
+        </h2>
+        <span className="text-info">{user?.name}</span>
+        <Nav>
+          <Stack direction="horizontal" gap={3}>
+            {user && (
+              <>
+                <Link
+                  onClick={() => logoutUser()}
+                  to="/login"
+                  className="link-light text-decoration-none"
+                >
+                  Выйти
+                </Link>
+              </>
+            )}
 
-    return <Navbar bg="dark" className="mb-4" style={{height: "3.75rem"}}>
-        <Container>
-            <h2>
-                <Link to="/" className="link-light text-decoration-none">Мессенджер</Link>
-            </h2>
-            <span className="text-info">{user?.name}</span>
-            <Nav>
-                <Stack direction="horizontal" gap={3}>
-                    <Link to="/login" className="link-light text-decoration-none">Войти</Link>
-                    <Link to="/register" className="link-light text-decoration-none">Зарегистрироваться</Link>
-                </Stack>
-            </Nav>
-        </Container>
-    </Navbar>;
-}
+            {!user && (
+              <>
+                <Link to="/login" className="link-light text-decoration-none">
+                  Войти
+                </Link>
+                <Link
+                  to="/register"
+                  className="link-light text-decoration-none"
+                >
+                  Зарегистрироваться
+                </Link>
+              </>
+            )}
+          </Stack>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavBar;
